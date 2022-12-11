e29be9ea000cc6cd17eac8883a309cdfae414d73
Bpar_Hapoda <cddvdrom@yandex.ru>
2022-12-11 14:11:22 +0300

    1

--- 7/2/Program.cs
+++ 7/2/Program.cs
@@ -9,3 +9,39 @@
 //Ряд > 1
 //Колонка > 7
 //1, 7 -> такого числа в массиве нет
+int[,] numbers =  {
+                       {1,4,7,2},
+                       {5,9,2,3},
+                       {8,4,2,4},
+                       };
+
+int rows = numbers.GetUpperBound(0) + 1;    // количество строк
+int columns = numbers.Length / rows;        // количество столбцов
+                                            // или так
+                                            // int columns = numbers.GetUpperBound(1) + 1;
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write($"{numbers[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+
+while (true)
+{
+    Console.WriteLine("Введите координаты элемента через , : (для выхода q");
+    string str = Console.ReadLine();
+    if (str.Equals("q")) { break; }
+    string[] str1 = new string[2];
+    str1 = str.Split(",");
+    int[] a = new int[2];
+    a[0] = Convert.ToInt32(str1[0]);
+    a[1] = Convert.ToInt32(str1[1]);
+    if ((a[0] < 0 || a[0] > columns - 1) || (a[1] < 0 || a[1] > rows - 1)) { Console.WriteLine("Мимо "); }
+    else
+    {
+        Console.WriteLine(numbers[a[0], a[1]]);
+    }
+}
