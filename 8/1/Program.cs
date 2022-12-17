0eceb078d78d5fcfccbdbbe921c9ac5a966127c4
Bpar_Hapoda <cddvdrom@yandex.ru>
2022-12-17 17:02:26 +0300

    1 task done

--- 8/1/Program.cs
+++ 8/1/Program.cs
@@ -10,7 +10,7 @@
 //8 4 4 2
 
 
-int [,] arr= getNewArray(5,5);
+int[,] arr = getNewArray(5, 5);
 Console.WriteLine("Исходный массив :");
 
 
@@ -19,46 +19,54 @@ Console.WriteLine("");
 Console.WriteLine("Массив со строками по убыванию :");
 printArray(minArray(arr));
 
-int [,] minArray (int [,]arr ){
-    int count;
-    int min=0;
-    int temp=0;
-    
-for(int i =0;i<arr.GetLength(0);i++){
-    count=1;
-    min=arr[i,0];
-    while (count !=0){
-        count=0;
-    for(int j=0;j<arr.GetLength(1)-1;j++){
-        if(arr[i,j+1]>arr[i,j]){temp=arr[i,j];arr[i,j]=arr[i,j+1];arr[i,j+1]=temp;count++;}
-    }
-    }
-}
-
-return arr;}
-
-
-void printArray(int [,] array)
+int[,] minArray(int[,] arr)
 {
+    int count;
+    int min = 0;
+    int temp = 0;
 
-    for (int i = 0; i < arr.GetLength(0); i++) {
-            for (int j = 0; j < arr.GetLength(1); j++)
-                Console.Write(arr[i, j] + " ");
-            Console.WriteLine();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        count = 1;
+        min = arr[i, 0];
+        while (count != 0)
+        {
+            count = 0;
+            for (int j = 0; j < arr.GetLength(1) - 1; j++)
+            {
+                if (arr[i, j + 1] > arr[i, j]) { temp = arr[i, j]; arr[i, j] = arr[i, j + 1]; arr[i, j + 1] = temp; count++; }
+            }
         }
     }
 
+    return arr;
+}
 
 
-int [,] getNewArray (int m,int n){
-int[,] array = new int[m, n];
-Random random = new Random();
-for (int i = 0; i < m; i++)
+void printArray(int[,] array)
 {
-    for (int j = 0; j < n; j++)
-    {
-        array[i, j] =(int) random.NextInt64(0,10);
 
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            Console.Write(arr[i, j] + " ");
+        Console.WriteLine();
     }
 }
-return array;}
+
+
+
+int[,] getNewArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    Random random = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = (int)random.NextInt64(0, 10);
+
+        }
+    }
+    return array;
+}
