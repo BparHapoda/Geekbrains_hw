4239c1428ffee9873a0a0f4eb807cb59c99ca48b
Bpar_Hapoda <cddvdrom@yandex.ru>
2022-12-21 02:08:33 +0300

    well done

--- 9/2/Program.cs
+++ 9/2/Program.cs
@@ -1,12 +1,12 @@
 ﻿//Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму
- //натуральных элементов в промежутке от M до N с помощью рекурсии.
+//натуральных элементов в промежутке от M до N с помощью рекурсии.
 //M = 1; N = 15 -> 120
 //M = 4; N = 8. -> 30
 
 int m = inputNumber("m");
 int n = inputNumber("n");
-int a=numbers(m, n,0);
-Console.WriteLine("M = "+m+"; N = "+n+" - > "+a);
+int a = numbers(m, n, 0);
+Console.WriteLine("M = " + m + "; N = " + n + " - > " + a);
 
 
 int inputNumber(String str)
@@ -17,16 +17,16 @@ int inputNumber(String str)
     return n;
 }
 
-int numbers(int m, int n,int sum)
+int numbers(int m, int n, int sum)
 {
-    
+
     if (m > n)
     {
-return sum;
+        return sum;
     }
-    
 
-    sum+=m;
+
+    sum += m;
     m++;
-    return numbers(m, n,sum);
+    return numbers(m, n, sum);
 }
--- 9/3/Program.cs
+++ 9/3/Program.cs
@@ -1,6 +1,27 @@
-﻿//Задача 3: Напишите программу вычисления функции Аккермана 
-//с помощью рекурсии. Даны два неотрицательных числа m и n.
+﻿//Задача 3: Напишите программу вычисления функции Аккермана с
+// помощью рекурсии. Даны два неотрицательных числа m и n.
 //m = 2, n = 3 -> A(m,n) = 9
 //m = 3, n = 2 -> A(m,n) = 29
+int m = inputNumber("m");
+int n = inputNumber("n");
+Console.WriteLine ("m = "+ m+", n = "+n+ " -> A(m,n) = "+ackerman(m,n));
 
 
+int inputNumber(String str)
+{
+    Console.WriteLine("Введите число " + str + " : ");
+    str = Console.ReadLine();
+    int n = Int32.Parse(str);
+    return n;
+}
+
+int ackerman (int m,int n){
+    if (m==0){return n+1;}
+    if (m > 0 && n == 0){
+return ackerman(m-1, 1);
+    }
+     if (m > 0 && n > 0) {
+    return ackerman(m-1, ackerman(m, n-1));
+  }
+  return 0;
+}
